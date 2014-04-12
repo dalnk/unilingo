4c044919eb3cc754fd53d7ce933858faac039e4d
Dennis Aleynikov <daleynikov@gmail.com>
2014-04-11 23:59:25 -0700

    Rails initial structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Unilingo::Application.config.secret_key_base = '4e4f9f2e63356a57986c66962e4cddc29b5d60856a53c823c75632a807ea1d052388c91d50e0d89c1c7bfe6d6fff7d97a1e0ec793e9a3d8afb01dbaad34fff87'
