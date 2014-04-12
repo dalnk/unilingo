e7ed089d9d13b33779b8e81703cd851d05122c65
Ho-Wei Kang <howeik@gmail.com>
2014-04-12 07:43:43 -0700

    added global live chat

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,7 @@ Unilingo::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # for websocket-rails: https://github.com/DanKnox/websocket-rails/wiki/Installation-and-Setup
+  config.middleware.delete Rack::Lock
 end
